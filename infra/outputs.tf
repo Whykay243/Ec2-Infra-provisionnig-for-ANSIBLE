cf7ef8a90e4d1860b1eb7febe9b4275d1d2cf691
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-18 13:42:57 -0400

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -4,15 +4,15 @@ output "private_key_pem" {
 }
 
 output "instance_public_ip_Nginx-server" {
-  value = aws_instance.Nginx-server.public_ip
+  value = aws_instance.nginx.public_ip
 }
 
 output "instance_public_ip_Apache-server" {
-  value = aws_instance.Apache-server.public_ip
+  value = aws_instance.apache.public_ip
 }
 
 output "instance_public_ip_Mysql-server" {
-  value = aws_instance.Mysql-server.public_ip
+  value = aws_instance.mysql.public_ip
 }
 output "public_key_openssh" {
   value = tls_private_key.ssh_key.public_key_openssh
